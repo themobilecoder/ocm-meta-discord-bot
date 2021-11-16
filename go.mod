54d8eac8c08d7a909c2e1718dff2bf3f7cc550df
Rafael Delos Santos <rkdelossantos@gmail.com>
2021-11-17 08:44:33 +1100

    Add env config

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/themobilecoder/ocm-meta-discord-bot
 go 1.17
 
 require github.com/themobilecoder/ocm-meta v0.0.0-20211116080552-93c35fdb40d3
+
+require github.com/kelseyhightower/envconfig v1.4.0
--- src/main.go
+++ src/main.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
 
+	"github.com/kelseyhightower/envconfig"
 	"github.com/themobilecoder/ocm-meta/meta"
 )
 
+type Config struct {
+	DISCORD_API_KEY string `envconfig:"DISCORD_API_KEY" required:"true"`
+	OCM_GUILD_ID    string `envconfig:"OCM_GUILD_ID" required:"true"`
+}
+
 func main() {
+	var cfg Config
+	err := envconfig.Process("METABOT", &cfg)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
 	monkeys := meta.GetOnChainMonkeys()
 	fmt.Print(monkeys[4642-1])
 }
